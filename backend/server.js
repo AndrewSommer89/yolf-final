f2f5513443295ccfbe57f32278dec1532b9fa629
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2019-08-10 15:37:41 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,11 @@ app.use(logger('dev'));
 console.log(path.join(__dirname, "../client/build"))
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static(path.join(__dirname, "../client/build.index.html")));
+    app.use(express.static(path.join(__dirname, "../client/build")));
     
     // Express serve up index.html file if it doesn't recognize route
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build.index.html"));
+    res.sendFile(path.join(__dirname, "../client/build"));
     });
     } else{
         app.use(express.static(path.join(__dirname, "../client/build")));
